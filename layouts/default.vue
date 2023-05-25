a4d2e96caa1dd14f665d0af857eb7d122e901be2
TomatoGuy0502 <H34066131@gs.ncku.edu.tw>
2023-05-26 00:43:16 +0100

    feat: Add dynamic router menu

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,13 +8,13 @@
       <label for="my-drawer" class="drawer-overlay"></label>
       <div
         class="flex flex-col gap-4 border-r-2 border-base-300 bg-base-200 p-4 text-base-content"
-        :class="[isExpanded ? 'w-60' : 'w-[84px]']"
+        :class="[isExpanded ? 'w-80' : 'w-[84px]']"
       >
         <ul class="menu gap-2">
           <li>
             <NuxtLink
               class="whitespace-nowrap rounded-lg leading-5"
-              to="/"
+              to="/tool"
               active-class="active"
             >
               <svg
@@ -31,32 +31,31 @@
                   d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
                 />
               </svg>
-              {{ isExpanded ? 'Item 1' : '' }}
+              {{ isExpanded ? 'Tool' : '' }}
             </NuxtLink>
           </li>
-          <li>
-            <NuxtLink
-              class="whitespace-nowrap rounded-lg leading-5"
-              to="/tutorial/basic"
-              active-class="active"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+        </ul>
+        <ul class="menu menu-compact bg-base-100 p-2 rounded-box">
+          <div
+            v-for="(section, i) in tutorialSectionNames"
+            :key="section"
+            class="collapse collapse-arrow"
+          >
+            <input type="checkbox" class="min-h-fit" checked />
+            <li class="collapse-title menu-title min-h-fit capitalize">
+              {{ i + 1 }}. {{ section }}
+            </li>
+            <div class="collapse-content">
+              <li
+                v-for="(route, ii) in tutorialRoutes[section]"
+                :key="route.name"
               >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-              {{ isExpanded ? 'Item 2' : '' }}
-            </NuxtLink>
-          </li>
+                <NuxtLink :to="route.path" active-class="active"
+                  >{{ i + 1 }}-{{ ii + 1 }}. {{ route.name }}</NuxtLink
+                >
+              </li>
+            </div>
+          </div>
         </ul>
         <button class="btn" type="button" @click="toggle">
           {{ isExpanded ? '←' : '→' }}
@@ -67,9 +66,43 @@
 </template>
 
 <script setup lang="ts">
-const isExpanded = ref(false)
+const isExpanded = ref(true)
 
 const toggle = () => {
   isExpanded.value = !isExpanded.value
 }
+
+interface TutorialRoute {
+  name: string
+  path: string
+}
+
+// Edit this array to add new tutorial routes
+const tutorialSectionNames = ['basic', 'representation'] as const
+
+const tutorialRoutes = tutorialSectionNames.reduce((acc, curr) => {
+  acc[curr] = []
+  return acc
+}, {} as { [key in (typeof tutorialSectionNames)[number]]: TutorialRoute[] })
+
+useRouter()
+  .options.routes.filter((route) => route.path.startsWith('/tutorial'))
+  .sort((a, b) => {
+    const aPageOrder = a.meta?.pageOrder as number
+    const bPageOrder = b.meta?.pageOrder as number
+    return aPageOrder - bPageOrder
+  })
+  .forEach((route) => {
+    const routeName = route.name as string
+    const routePath = route.path as string
+    if (routePath.startsWith('/tutorial')) {
+      const tutorialRouteName = routePath.split(
+        '/'
+      )[2] as (typeof tutorialSectionNames)[number]
+      tutorialRoutes[tutorialRouteName].push({
+        name: routeName,
+        path: routePath,
+      })
+    }
+  })
 </script>
---
+++ pages/tutorial/basic/1-vertex-and-edge.vue
@@ -0,0 +1,256 @@
+<template>
+  <div class="flex gap-4 p-4">
+    <div>
+      <button class="btn-ghost btn" @click="clearData">Clear</button>
+      <svg ref="svg" :width="width" :height="height" class="cursor-cell">
+        <line
+          class="draw-edge cursor-cell stroke-[3]"
+          :class="[mousedownNode ? 'stroke-current' : 'stroke-none']"
+          :x1="drawEdgeCords.x1"
+          :y1="drawEdgeCords.y1"
+          :x2="drawEdgeCords.x2"
+          :y2="drawEdgeCords.y2"
+        ></line>
+        <g class="edges"></g>
+        <g class="nodes"></g>
+        <g class="text"></g>
+      </svg>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted, reactive } from 'vue'
+import * as d3 from 'd3'
+
+definePageMeta({
+  name: 'Vertex and Edge',
+  path: '/tutorial/basic/vertex-and-edge',
+  pageOrder: 1,
+})
+
+const svg = ref<SVGSVGElement | null>(null)
+
+interface NodeData extends d3.SimulationNodeDatum {
+  id: number
+}
+
+interface EdgeData extends d3.SimulationLinkDatum<NodeData> {}
+
+interface GraphData {
+  nodes: NodeData[]
+  edges: EdgeData[]
+  set: Set<string>
+}
+
+const data = reactive<GraphData>({
+  nodes: [{ id: 0 }, { id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }],
+  edges: [],
+  set: new Set(),
+})
+const nextId = ref(data.nodes.length)
+
+const width = 600
+const height = 600
+
+const $svg = d3.select(svg.value)
+let $edge = $svg.select('.edges').selectAll<SVGLineElement, EdgeData>('line')
+let $node = $svg
+  .select('.nodes')
+  .selectAll<SVGCircleElement, NodeData>('circle')
+let $text = $svg.select('.text').selectAll<SVGTextElement, NodeData>('text')
+
+const simulation = d3
+  .forceSimulation<NodeData, EdgeData>(data.nodes)
+  .force('link', d3.forceLink(data.edges).distance(40))
+  .force(
+    'charge',
+    d3
+      .forceManyBody()
+      .strength(-160)
+      .distanceMax(width / 2)
+  )
+  .force('center', d3.forceCenter(width / 2, height / 2))
+  .force('x', d3.forceX(width / 2))
+  .force('y', d3.forceY(height / 2))
+  .on('tick', ticked)
+
+onMounted(() => {
+  const $svg = d3
+    .select(svg.value)
+    .on('mousedown', addNode)
+    .on('contextmenu', (event: PointerEvent | MouseEvent) =>
+      event.preventDefault()
+    )
+    .on('mousemove', updateDrawEdge)
+    .on('mouseup mouseleave', hideDrawEdge)
+    .attr('viewBox', [0, 0, width, height])
+    .attr('width', width)
+    .attr('height', height)
+
+  $edge = $svg.select('.edges').selectAll<SVGLineElement, EdgeData>('line')
+  $node = $svg.select('.nodes').selectAll<SVGCircleElement, NodeData>('circle')
+  $text = $svg.select('.text').selectAll<SVGTextElement, NodeData>('text')
+
+  updateSVG()
+})
+
+function ticked() {
+  $edge
+    .attr('x1', (d) => (d.source as NodeData).x ?? 0)
+    .attr('y1', (d) => (d.source as NodeData).y ?? 0)
+    .attr('x2', (d) => (d.target as NodeData).x ?? 0)
+    .attr('y2', (d) => (d.target as NodeData).y ?? 0)
+
+  $node.attr('cx', (d) => d.x ?? 0).attr('cy', (d) => d.y ?? 0)
+
+  $text.attr('x', (d) => d.x ?? 0).attr('y', (d) => d.y ?? 0)
+}
+
+function updateSVG() {
+  $edge = $edge
+    .data(data.edges)
+    .join('line')
+    .on('contextmenu', removeEdge)
+    .on('mousedown', (event) => event.stopPropagation())
+  $node = $node
+    .data(data.nodes)
+    .join('circle')
+    .style('fill', (d) => d3.schemeTableau10[d.id % 10])
+    .on('contextmenu', removeNode)
+    .on('mousedown', beginDrawEdge)
+    .on('mouseup', endDrawEdge)
+    .on('mouseenter', highlightNode)
+    .on('mouseleave', unhighlightNode)
+  $text = $text
+    .data(data.nodes)
+    .join('text')
+    .attr('dx', 12)
+    .attr('dy', 6)
+    .text((d) => d.id)
+  simulation.force('link', d3.forceLink(data.edges).distance(40))
+  simulation.nodes(data.nodes)
+  simulation.alpha(0.8).restart()
+}
+
+function clearData() {
+  nextId.value = 0
+  data.nodes.length = 0
+  data.edges.length = 0
+  data.set.clear()
+  updateSVG()
+}
+
+// Edit Node
+function addNode(event: PointerEvent | MouseEvent) {
+  if (event.button !== 0) return
+  const [x, y] = d3.pointer(event)
+  data.nodes.push({ id: nextId.value++, x, y })
+  updateSVG()
+}
+
+function removeNode(event: PointerEvent | MouseEvent, d: NodeData) {
+  event.preventDefault()
+  data.nodes.splice(data.nodes.indexOf(d), 1)
+  data.edges = data.edges.filter(
+    (edge) => edge.source !== d && edge.target !== d
+  )
+  data.set.clear()
+  data.edges.forEach((edge) => {
+    data.set.add(
+      `${(edge.source as NodeData).index},${(edge.target as NodeData).index}`
+    )
+  })
+  updateSVG()
+}
+const hoverNode = ref<NodeData | null>(null)
+function highlightNode(_event: PointerEvent | MouseEvent, d: NodeData) {
+  hoverNode.value = d
+}
+function unhighlightNode() {
+  hoverNode.value = null
+}
+
+// Edit Edge
+const mousedownNode = ref<NodeData | null>(null)
+const drawEdgeCords = reactive({
+  x1: 0,
+  y1: 0,
+  x2: 0,
+  y2: 0,
+})
+
+function beginDrawEdge(event: PointerEvent | MouseEvent, d: NodeData) {
+  event.preventDefault()
+  event.stopPropagation()
+  mousedownNode.value = d
+  const [x, y] = d3.pointer(event)
+  drawEdgeCords.x1 = d.x ?? x
+  drawEdgeCords.y1 = d.y ?? y
+  drawEdgeCords.x2 = x
+  drawEdgeCords.y2 = y
+}
+function updateDrawEdge(event: PointerEvent | MouseEvent) {
+  if (!mousedownNode.value) return
+  const [x, y] = d3.pointer(event)
+  drawEdgeCords.x1 = mousedownNode.value?.x ?? x
+  drawEdgeCords.y1 = mousedownNode.value?.y ?? y
+  drawEdgeCords.x2 = x
+  drawEdgeCords.y2 = y
+}
+function endDrawEdge(_event: PointerEvent | MouseEvent, d: NodeData) {
+  if (!mousedownNode.value || d === mousedownNode.value) return
+  const sourceId = mousedownNode.value?.id
+  const targetId = d.id
+  if (
+    data.set.has(`${sourceId},${targetId}`) ||
+    data.set.has(`${targetId},${sourceId}`)
+  )
+    return
+  data.edges.push({ source: mousedownNode.value, target: d })
+  data.set.add(`${sourceId},${targetId}`)
+  updateSVG()
+}
+function hideDrawEdge() {
+  mousedownNode.value = null
+}
+function removeEdge(event: PointerEvent | MouseEvent, d: EdgeData) {
+  event.preventDefault()
+  data.edges.splice(data.edges.indexOf(d), 1)
+  data.set.delete(
+    `${(d.source as NodeData).index},${(d.target as NodeData).index}`
+  )
+  updateSVG()
+}
+</script>
+
+<style scoped>
+svg {
+  background-color: #eee;
+}
+svg > line {
+  stroke-linecap: round;
+  stroke-linejoin: round;
+}
+.edges :deep(line) {
+  stroke: #000;
+  stroke-width: 3px;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+}
+.edges :deep(line):hover {
+  stroke: #f00;
+  stroke-width: 4px;
+  cursor: pointer;
+}
+.nodes :deep(circle) {
+  r: 10;
+}
+.nodes :deep(circle):hover {
+  opacity: 0.8;
+  cursor: pointer;
+}
+.text :deep(text) {
+  user-select: none;
+}
+</style>

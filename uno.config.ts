083904d39d3d94a959cc9e23fe9f45218ba98f96
TomatoGuy0502 <H34066131@gs.ncku.edu.tw>
2023-07-22 00:05:10 +0100

    fix: Adjust overflow in tutorial/algorithm

--- uno.config.ts
+++ uno.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, presetIcons } from 'unocss'
 export default defineConfig({
   presets: [
     presetIcons({
-      autoInstall: true,
+      // autoInstall: true,
     }),
   ],
 })
